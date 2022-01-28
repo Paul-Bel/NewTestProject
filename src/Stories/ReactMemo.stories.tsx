14f60b6e9bac2891418e3481d9aa21eb69960aa6
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-01-28 12:37:53 +0300

    add React.memo(CounterSecret)

--- src/Stories/ReactMemo.stories.tsx
+++ src/Stories/ReactMemo.stories.tsx
@@ -4,12 +4,23 @@ export default {
     title: 'ReactMemoDemo'
 }
 
-const Counter = (props: { count: number }) => {
+const CounterSecret = (props: { count: number }) => {
+    console.log('Counter')
     return <div>{props.count}</div>
 }
-const UsersSecret = (props: { users: Array<string> }) => {
+const Counter = React.memo(CounterSecret)
+const UsersSecret = (props: { users: Array<string>,
+    value: string,
+    setValue: (value: string) => void
+    addUsers: () => void
+}) => {
     console.log('users')
     return <div>
+        <input
+        value={props.value}
+        onChange={(e)=>props.setValue(e.currentTarget.value)}
+        />
+        <button onClick={props.addUsers}>ADD USER</button>
         {props.users.map((m, i) => <div key={i}>{m}</div>)}
     </div>
 }
@@ -18,9 +29,14 @@ const Users = React.memo(UsersSecret)
 export const Example1 = () => {
     const [counter, setCounter] = useState(0)
     const [users, setUsers] = useState(['Dima', 'Vitya', 'Kolya'])
+    const [value, setValue] = useState('')
+    const addUsers = () => {
+        setUsers([...users, value]);
+        setValue('')
+    }
     return <>
         <button onClick={()=>setCounter(counter+1)}>{counter}</button>
         <Counter count={counter}/>
-        <Users users={users}/>
+        <Users users={users} value={value} setValue={setValue} addUsers={addUsers}/>
     </>
 }
