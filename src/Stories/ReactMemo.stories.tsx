1fb8dbdf2f78c59aa04250e9d7e2c601ee842cc7
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-01-28 12:26:18 +0300

    add story components

--- src/Stories/ReactMemo.stories.tsx
+++ src/Stories/ReactMemo.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default {
     title: 'ReactMemoDemo'
@@ -7,15 +7,18 @@ export default {
 const Counter = (props: { count: number }) => {
     return <div>{props.count}</div>
 }
-const Users = (props: {users: Array<string>}) => {
+const Users = (props: { users: Array<string> }) => {
+    console.log('users')
     return <div>
         {props.users.map((m, i) => <div key={i}>{m}</div>)}
     </div>
 }
 export const Example1 = () => {
-
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Dima', 'Vitya', 'Kolya'])
     return <>
-        <Counter count={10}/>
-        <Users users={['Dima', 'Vitya', 'Kolya']}/>
+        <button onClick={()=>setCounter(counter+1)}>{counter}</button>
+        <Counter count={counter}/>
+        <Users users={users}/>
     </>
 }
